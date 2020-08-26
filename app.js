7627cb6db523562132f684b8d7947fe4199007cf
MrMrP <marcus.l.price@icloud.com>
2020-08-26 00:38:57 -0400

    "beginning to make the 'tetris' shapes"

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const Startbtn = document.querySelector('#start-button')
+  const width = 10
 
+  //The Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+3]
+  ]
+
+  const tTetromino = [
+    [1, width, width+1, width+2],
+    [1, width+1,width+2,width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1, width, width+1],
+    [0,1, width, width+1],
+    [0,1, width, width+1],
+    [0,1, width, width+1]
+  ]
+
+  const iTetromino = [
+    [1, width+1,width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ]
+
+  const theTetrominoes = [lTetromino, tTetromino, oTetromino, iTetromino]
+
+  let currentPosition = 4
+  let current = theTetrominoes[0][0]
+  console.log(theTetrominoes)
 })
